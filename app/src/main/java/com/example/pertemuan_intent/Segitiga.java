c16058ec010720c10cbe110599f03e9f66eab2c3
Raflizar Deswandi Yahya <raflizardeswandiy24@gmail.com>
2022-10-08 13:49:11 +0700

    edit validation

--- app/src/main/java/com/example/pertemuan_intent/Segitiga.java
+++ app/src/main/java/com/example/pertemuan_intent/Segitiga.java
@@ -9,47 +9,40 @@ import android.widget.EditText;
 
 public class Segitiga extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_segitiga);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_segitiga);
 
-        final EditText alas = (EditText) findViewById(R.id.input_alas);
-        final EditText tinggi = (EditText) findViewById(R.id.input_tinggi);
-        final EditText luas = (EditText) findViewById(R.id.output_hasil);
-        final Button hitung = (Button) findViewById(R.id.btn_hitung);
+    final EditText alas = findViewById(R.id.input_alas);
+    final EditText tinggi = findViewById(R.id.input_tinggi);
+    final EditText luas = findViewById(R.id.output_hasil);
+    final Button hitung =  findViewById(R.id.btn_hitung);
 
-        hitung.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String inputAlas = alas.getText().toString();
-                String inputTinggi = tinggi.getText().toString();
+    hitung.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String inputAlas = alas.getText().toString();
+        String inputTinggi = tinggi.getText().toString();
 
-                try {
-                    if(inputAlas.isEmpty()) {
-                        alas.setError("Alas tidak boleh kosong");
-                    }
+        try {
 
-                    if(inputTinggi.isEmpty()) {
-                        tinggi.setError("Tinggi tidak boleh kosong");
-                    }
+          if(!inputAlas.matches("[0-9]+")) {
+            alas.setError("Alas harus berupa angka");
+          }
 
-                    if(!inputAlas.matches("[0-9]+")) {
-                        alas.setError("Alas harus berupa angka");
-                    }
+          if(!inputTinggi.matches("[0-9]+")) {
+            tinggi.setError("Tinggi harus berupa angka");
+          }
 
-                    if(!inputTinggi.matches("[0-9]+")) {
-                        tinggi.setError("Tinggi harus berupa angka");
-                    }
-
-                    double a = Double.parseDouble(inputAlas);
-                    double t = Double.parseDouble(inputTinggi);
-                    double hasil = 0.5 * a * t;
-                    luas.setText(String.valueOf(hasil));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+          double a = Double.parseDouble(inputAlas);
+          double t = Double.parseDouble(inputTinggi);
+          double hasil = 0.5 * a * t;
+          luas.setText(String.valueOf(hasil));
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
 }
